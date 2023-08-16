4aed7c4704828a87481509cdc52538a42f4be21a
RSushil1 <sushilsinghrathore1998@gmail.com>
2023-08-16 17:04:30 +0530

    ,,

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express= require('express');
+const http = require ('http');
 const { Server } = require("socket.io");
-const { parse } = require('url');
 const cors= require("cors");
 
 
@@ -22,48 +22,41 @@ app.use(express.json());
 
 
 
-
+const server = http.createServer(app); // Create HTTP server instance
 //routes
 app.use("/socket.io", (req, res) => {
-  const { query } = parse(req.url, true);
-  const id = query.id;
-
-  if (!id) {
-    res.statusCode = 400;
-    res.end('Missing "id" parameter in the query string');
-    return;
+  // Socket.IO configuration
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['my-custom-header'],
+    credentials: true
   }
+}); // Pass the HTTP server instance to the Socket.IO Server
 
-  const io = new Server({
-    cors: {
-      origin: "https://echochat.vercel.app",
-      allowedHeaders: ["my-custom-header"],
-      credentials: true
-    }
-  });
-
-  io.on('connection', socket => {
-    socket.join(id);
-    console.log(id);
-
-    socket.on('send-message', ({ recipients, text }) => {
+io.on('connection', socket => {
+  const id = socket.handshake.query.id;
+  socket.join(id);
+  
+  socket.on('send-message', ({ recipients, text }) => {
       recipients.forEach(recipient => {
-        const newRecipients = recipients.filter(r => r !== recipient);
-        newRecipients.push(id);
-        io.to(recipient).emit('receive-message', {
-          recipients: newRecipients,
-          sender: id,
-          content: text,
-          timestamp: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
-        });
+          const newRecipients = recipients.filter(r => r !== recipient);
+          newRecipients.push(id);
+          io.to(recipient).emit('receive-message', {
+              recipients: newRecipients,
+              sender: id,
+              content: text,
+              timestamp: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
+          });
       });
-    });
   });
+});
 
   io.listen(req, res)
 });
 
 //run listen
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server Running on mode on port ${PORT}`);
 });
